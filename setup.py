c199f0ccc03f36e9498f00fbb0918b7e0dff983e
Dominic Fitzgerald <djf604@truman.edu>
2016-03-20 21:58:21 -0500

    Added setup script

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='Chunky',
+    version='0.1.0',
+    description='Pipeline design and distribution framework',
+    author='Dominic Fitzgerald',
+    author_email='dominicfitzgerald11@gmail.com',
+    url='https://github.com/djf604/chunky',
+    packages=['chunky'],
+    entry_points={
+        'console_scripts': ['chunky = chunky.util:execute_from_command_line']
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development :: Libraries'
+    ]
+)

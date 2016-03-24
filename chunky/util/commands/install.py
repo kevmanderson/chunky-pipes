ef7426d5caa0f29f65377101a3c8f221ed0b7bba
tmg2134 <tmg2134@truman.edu>
2016-03-24 18:40:44 -0500

    Now uses shutil.copy2() instead, more agnostic.

--- chunky/util/commands/install.py
+++ chunky/util/commands/install.py
@@ -1,53 +1,57 @@
-import subprocess
 import sys
 import os
+import shutil
 from chunky.util.base import BaseCommand
 
 ARGV_PIPELINE_NAME = 0
+EXIT = 0;
 EXIT_CMD_SYNTAX_ERROR = 2
 
 
 class Command(BaseCommand):
     def usage(self):
-        return 'Easily install pipelines'
+        return 'Usage: chunky install <pipeline>'
 
     def help_text(self):
-        return ''
+        return 'Install a new pipeline\n ' \
+               'Example: chunky install my_pipeline\n' \
+               'Do not include extensions'
 
     def run(self):
         # Checks to see if they entered a pipeline name
         if not self.argv:
-            sys.stdout.write('No pipeline provided to install.\n')
+            #sys.stdout.write('No pipeline provided to install.\n')
             sys.stdout.write(self.usage() + '\n')
             sys.exit(EXIT_CMD_SYNTAX_ERROR)
 
         # Get pipeline name from argv
         pipeline_name = self.argv[ARGV_PIPELINE_NAME]
 
+        if os.path.isfile('{}.py'.format(pipeline_name)) is False:
+            sys.stdout.write('Pipeline not found\n')
+            sys.exit(EXIT)
+
         # Helper to check if pipeline exists
         pipeline_filepath = os.path.join(self.home_pipelines,
-                                         '{}'.format(pipeline_name))
+                                         '{}.py'.format(pipeline_name))
 
         # Check if pipeline already exists
-        if os.path.exists(pipeline_filepath):
+        if os.path.isfile(pipeline_filepath):
             overwrite = raw_input('Pipeline {} is already installed, overwrite? [y/n] '.format(
                     pipeline_name
             ))
             no_set = {'no', 'n'}
             # Terminate if user says no
             if overwrite.lower() in no_set:
-                sys.exit(0)
-
-        # Not quite sure how to get rid of the:
-        # 'cp: cannot stat 'non_existant_file': No such file or directory' error
+                sys.exit(EXIT)
 
         # Make sure they don't input the wrong name
         try:
-            subprocess.check_call(['cp', pipeline_name, self.home_pipelines])
-            sys.stdout.write('Pipeline successfully installed\n')
-        except (IOError, OSError, subprocess.CalledProcessError):
-            sys.stdout.write('Pipeline not found\n')
-            sys.exit(0)
+            shutil.copy2('{}.py'.format(pipeline_name), self.home_pipelines)
+            sys.stdout.write('Pipeline {} successfully installed\n'.format(pipeline_name))
+        except (IOError, OSError, shutil.Error):
+            sys.stdout.write('Pipeline {} not found\n'.format(pipeline_name))
+            sys.exit(EXIT)
 
 
 

021ab0b6fdcc32a9e6d9350de1226767e42d7639
tmg2134 <tmg2134@truman.edu>
2016-03-24 20:16:42 -0500

    List should work now, took away the .py extensions in install

--- chunky/util/commands/install.py
+++ chunky/util/commands/install.py
@@ -4,18 +4,17 @@ import shutil
 from chunky.util.base import BaseCommand
 
 ARGV_PIPELINE_NAME = 0
-EXIT = 0;
+EXIT = 0
 EXIT_CMD_SYNTAX_ERROR = 2
 
 
 class Command(BaseCommand):
     def usage(self):
-        return 'Usage: chunky install <pipeline>'
+        return 'Usage: chunky install <pipeline>.'
 
     def help_text(self):
         return 'Install a new pipeline\n ' \
-               'Example: chunky install my_pipeline\n' \
-               'Do not include extensions'
+               'Example: chunky install my_pipeline.py\n'
 
     def run(self):
         # Checks to see if they entered a pipeline name
@@ -27,13 +26,13 @@ class Command(BaseCommand):
         # Get pipeline name from argv
         pipeline_name = self.argv[ARGV_PIPELINE_NAME]
 
-        if os.path.isfile('{}.py'.format(pipeline_name)) is False:
+        if os.path.isfile('{}'.format(pipeline_name)) is False:
             sys.stdout.write('Pipeline not found\n')
             sys.exit(EXIT)
 
         # Helper to check if pipeline exists
         pipeline_filepath = os.path.join(self.home_pipelines,
-                                         '{}.py'.format(pipeline_name))
+                                         pipeline_name)
 
         # Check if pipeline already exists
         if os.path.isfile(pipeline_filepath):
@@ -47,7 +46,7 @@ class Command(BaseCommand):
 
         # Make sure they don't input the wrong name
         try:
-            shutil.copy2('{}.py'.format(pipeline_name), self.home_pipelines)
+            shutil.copy2(pipeline_name, self.home_pipelines)
             sys.stdout.write('Pipeline {} successfully installed\n'.format(pipeline_name))
         except (IOError, OSError, shutil.Error):
             sys.stdout.write('Pipeline {} not found\n'.format(pipeline_name))

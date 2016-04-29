1ceb74dbee0555d027349b11a4bf4fb91539001c
Dominic Fitzgerald <djf604@truman.edu>
2016-04-29 15:09:05 -0500

    Removed __init__ from list subcommand

--- chunkypipes/util/commands/list.py
+++ chunkypipes/util/commands/list.py
@@ -2,6 +2,9 @@ import os
 import sys
 from chunkypipes.util.base import BaseCommand
 
+BASENAME = 0
+EXTENTION = 1
+
 
 class Command(BaseCommand):
 
@@ -15,24 +18,44 @@ class Command(BaseCommand):
     def run(self):
         sys.stdout.write('Installed pipelines (in {}):\n\n'.format(self.home_pipelines))
 
-        # Grab the pipelines from both directories
-        configured_pipelines = os.listdir(self.home_configs)
-        all_pipelines = os.listdir(self.home_pipelines)
-        pipeline_configuration = {}  # Dictionary for the pipelines
-        j = ''  # variable to keep out duplicates
+        # Grab the installed files from both directories
+        installed_pipelines = {os.path.splitext(pipeline)[BASENAME] for pipeline
+                               in os.listdir(self.home_pipelines)
+                               if pipeline != '__init__.py'}
 
-        # sort to avoid putting duplicates into dictionary
-        all_pipelines.sort()
+        installed_configs = [config for config in os.listdir(self.home_configs)]
 
-        # Put all pipelines in the library
-        for i in all_pipelines:
-            if j.split('.')[0] != i.split('.')[0]:  # if the previous pipeline has the same name, do not add
-                pipeline_configuration[i.split('.')[0]] = 'NOT configured'
-            j = i
+        for pipeline_name in installed_pipelines:
+            if '{}.json'.format(pipeline_name) in installed_configs:
+                print '{} is configured'.format(pipeline_name)
+            else:
+                print '{} is not configured'.format(pipeline_name)
 
-        # Set the pipelines in the configured folder to configured
-        for pipeline in configured_pipelines:
-            pipeline_configuration[pipeline.split('.')[0]] = 'configured'
-        # print the pipelines
-        for pipeline, configuration in pipeline_configuration.iteritems():
-            sys.stdout.write('{} is {}\n'.format(pipeline, configuration))
+        # return
+        #
+        #
+        # all_pipelines = [os.path.basename(pipeline) for pipeline
+        #                  in os.listdir(self.home_pipelines)
+        #                  if os.path.splitext(pipeline)[EXTENTION] != '.pyc']
+        # if '__init__.py' in all_pipelines:
+        #     all_pipelines.remove('__init__.py')
+        # print all_pipelines
+        # return
+        # pipeline_configuration = {}  # Dictionary for the pipelines
+        # j = ''  # variable to keep out duplicates
+        #
+        # # sort to avoid putting duplicates into dictionary
+        # all_pipelines.sort()
+        #
+        # # Put all pipelines in the library
+        # for i in all_pipelines:
+        #     if j.split('.')[0] != i.split('.')[0]:  # if the previous pipeline has the same name, do not add
+        #         pipeline_configuration[i.split('.')[0]] = 'NOT configured'
+        #     j = i
+        #
+        # # Set the pipelines in the configured folder to configured
+        # for pipeline in configured_pipelines:
+        #     pipeline_configuration[pipeline.split('.')[0]] = 'configured'
+        # # print the pipelines
+        # for pipeline, configuration in pipeline_configuration.iteritems():
+        #     sys.stdout.write('{} is {}\n'.format(pipeline, configuration))
